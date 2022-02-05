7d86213231f4d2cd3a5722b7e2e36de3cc3341e3
Anatoly Vdovichev <anatoly@clearscale.net>
2022-02-05 04:19:09 -0800

    - migration to androidx - dependencies update

--- app/src/main/java/com/example/glidetest/MainActivity.java
+++ app/src/main/java/com/example/glidetest/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.glidetest;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatTextView;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
